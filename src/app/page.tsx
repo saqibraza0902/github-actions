add65c67148fa879e0a43d4a4075c30ef0e2bd8f
unknown <saqibraza0902@gmail.com>
2024-06-07 16:33:33 +0500

    changing cothers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   const tokn = "VXf6FXkrAmPO1BMAt8vdbiuV";
-  return <div>This is home page</div>;
+  return <div>This is home page Poti bahi</div>;
 };
 
 export default Home;
