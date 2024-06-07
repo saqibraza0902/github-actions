334587cc5e10c92a8eb9523bb391c214c6e0c316
unknown <saqibraza0902@gmail.com>
2024-06-07 18:55:55 +0500

    setting envs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   const tokn = "VXf6FXkrAmPO1BMAt8vdbiuV";
-  return <div>This is home page Poti bahi</div>;
+  return <div>This is home page {process.env.NEXT_PUBLIC_NAME}</div>;
 };
 
 export default Home;
